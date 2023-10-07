0f6d517c16c30232cd159ad99d29c029947c6828
Rafael <rafaelandersonpg@gmail.com>
2023-10-07 12:50:13 -0500

    feat: facturas POO

---
+++ src/Facturas/Factura.java
@@ -0,0 +1,64 @@
+package Facturas;
+
+import java.util.Date;
+
+public class Factura {
+    private int folio;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private ItemFactura[] items;
+    private int indiceItems;
+    public static final int MAX_ITEMS = 12;
+    private static int ultimoFolio;
+
+    public Factura(String descripcion, Cliente cliente) {
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+        this.items = new ItemFactura[MAX_ITEMS];
+        this.folio = ++ultimoFolio;
+        this.fecha = new Date();
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItems() {
+        return items;
+    }
+
+    public void addItemFactura(ItemFactura item) {
+        if (indiceItems < MAX_ITEMS) {
+            this.items[this.indiceItems++] = item;
+        }
+    }
+}
