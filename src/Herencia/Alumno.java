80cacddcacff00633cf92231354fe9b16b7cb523
Rafael <rafaelandersonpg@gmail.com>
2023-10-27 03:09:29 -0500

    feat: herencia poo

---
+++ src/Herencia/Alumno.java
@@ -0,0 +1,65 @@
+package Herencia;
+
+public class Alumno extends Persona {
+    private String institucion;
+    private double notaMatematica;
+    private double notaCastellano;
+    private double notaHistoria;
+
+    public Alumno() {
+        System.out.println("Alumno: inicializando constructor");
+    }
+
+    public Alumno(String nombre, String apellido) {
+        super(nombre, apellido); // Referencia a constructor del padre
+    }
+
+    public Alumno(String nombre, String apellido, int edad) {
+        super(nombre, apellido, edad);
+    }
+
+    public Alumno(String nombre, String apellido, int edad, String institucion) {
+        super(nombre, apellido, edad);
+        this.institucion = institucion;
+    }
+
+    public Alumno(String nombre, String apellido, int edad, String institucion,
+                  double notaMatematica, double notaCastellano, double notaHistoria) {
+        this(nombre, apellido, edad, institucion);
+        this.notaMatematica = notaMatematica;
+        this.notaCastellano = notaCastellano;
+        this.notaHistoria = notaHistoria;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public double getNotaMatematica() {
+        return notaMatematica;
+    }
+
+    public void setNotaMatematica(double notaMatematica) {
+        this.notaMatematica = notaMatematica;
+    }
+
+    public double getNotaCastellano() {
+        return notaCastellano;
+    }
+
+    public void setNotaCastellano(double notaCastellano) {
+        this.notaCastellano = notaCastellano;
+    }
+
+    public double getNotaHistoria() {
+        return notaHistoria;
+    }
+
+    public void setNotaHistoria(double notaHistoria) {
+        this.notaHistoria = notaHistoria;
+    }
+}
---
+++ src/Herencia/AlumnoInternacional.java
@@ -0,0 +1,35 @@
+package Herencia;
+
+public class AlumnoInternacional extends Alumno {
+    private String pais;
+    private double notaIdiomas;
+
+    public AlumnoInternacional() {
+        System.out.println("AlumnoInternacional: inicializando constructor");
+    }
+
+    public AlumnoInternacional(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+    public AlumnoInternacional(String nombre, String apellido, String pais) {
+        super(nombre, apellido);
+        this.pais = pais;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public double getNotaIdiomas() {
+        return notaIdiomas;
+    }
+
+    public void setNotaIdiomas(double notaIdiomas) {
+        this.notaIdiomas = notaIdiomas;
+    }
+}
